58119812060f69d1e77103645a828645842fbe17
vineethakonakanchi <69679445+vineethakonakanchi@users.noreply.github.com>
2021-06-17 16:12:49 +0530

    Update Complex.java

--- JAVA PROGRAMMING/Complex.java
+++ JAVA PROGRAMMING/Complex.java
@@ -3,9 +3,7 @@ public class Complex {
    double real;
    
   double imag;
-
-  
-  
+   
   public Complex(double real, double imag) {
   
     this.real = real;
@@ -13,9 +11,7 @@ public class Complex {
     this.imag = imag;
     
   }
-
-  
-  
+   
   public static void main(String[] args) {
   
     Complex n1 = new Complex(2.3, 4.5),
@@ -23,32 +19,23 @@ public class Complex {
     n2 = new Complex(3.4, 5.0),
     
     temp;
-
-    
-    
+     
     temp = add(n1, n2);
-
-    
-    
+     
     System.out.printf("Sum = %.1f + %.1fi", temp.real, temp.imag);
     
   }
-
-  
+   
   public static Complex add(Complex n1, Complex n2)
   
   {
   
     Complex temp = new Complex(0.0, 0.0);
-
-    
-    
+   
     temp.real = n1.real + n2.real;
     
     temp.imag = n1.imag + n2.imag;
-
-    
-    
+   
     return(temp);
     
   }
