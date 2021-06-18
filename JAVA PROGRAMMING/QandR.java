09bf957fb7692daa066382f530bd13c478a78221
vineethakonakanchi <69679445+vineethakonakanchi@users.noreply.github.com>
2021-06-18 11:16:41 +0530

    Create QandR.java

---
+++ JAVA PROGRAMMING/QandR.java
@@ -0,0 +1,12 @@
+class QandR{
+public static void main(String[] args){
+System.out.println("Enter 2 numbers:");
+int a,b,q,r;
+a=100;
+b=20;
+q=a/b;
+r=a%b;
+System.out.println("quotient:"+q);
+System.out.println("remainder:"+r);
+}
+}
